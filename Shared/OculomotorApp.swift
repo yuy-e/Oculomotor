54504e74262771a76ffc1806c7f941e5b5c9c88b
sj346 <sj346@duke.edu>
2022-07-12 19:55:23 -0400

    Initial Commit

---
+++ Shared/OculomotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OculomotorApp.swift
+//  Shared
+//
+//  Created by loaner on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct OculomotorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
