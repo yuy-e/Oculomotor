32efd68a43d06855f62b0cf68dbce22d61118bf6
XG <41360561+Charlie059@users.noreply.github.com>
2022-07-14 08:49:55 -0400

    Add Pods, finish firebase registerVM

---
+++ Shared/ViewModel/RegisterViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewModel.swift
+//  Oculomotor (iOS)
+//
+//  Created by xg on 7/14/22.
+//
+
+import Foundation
+import Firebase
+
+class RegisterViewModel: ObservableObject{
+    var email: String = ""
+    var password: String = ""
+    
+    func register(completion: @escaping () -> Void) {
+        
+        Firebase.Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                completion()
+            }
+            
+        }
+        
+    }
+}
