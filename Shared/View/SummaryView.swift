4868eacb04b0da98bacbae5cec1461631c3c1df2
your_username <your_email@gmail.com>
2022-07-15 18:41:04 -0400

    My branch's first commit

--- Shared/View/SummaryView.swift
+++ Shared/View/SummaryView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct SummaryView: View {
+    @State private var darkMode: Bool = true
+    @State private var date = Date()
     var body: some View {
-        Text("Summary Screen goes here")
+        ScrollView {
+            VStack {
+                HStack{
+                    Text("Summary")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(systemName: "chart.pie")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.blue)
+                }
+                .padding(.all)
+                
+                VStack {
+                    DatePicker(
+                        "Start Date",
+                        selection: $date,
+                        displayedComponents: [.date, .hourAndMinute]
+                    )
+                    .datePickerStyle(.graphical)
+                .accentColor(.green)
+                }
+                Spacer()
+                Button("DarkMode", action: {
+                    darkMode.toggle()
+                })
+            }
+        }
+        .preferredColorScheme(darkMode ? .dark : .light)
     }
 }
 
+let darkMode: Bool  = true
 struct SummaryView_Previews: PreviewProvider {
     static var previews: some View {
         SummaryView()
